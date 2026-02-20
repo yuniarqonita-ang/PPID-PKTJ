78e93649b1533863f3d0090f985b8f4413c25ba4
4n6 <yuniarqonita@gmail.com>
2026-02-20 09:16:36 +0700

    fix: remove duplicate route registration in bootstrap/app.php that was causing service container errors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,19 +3,12 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
-        then: function () {
-            // Ini kuncinya: Memaksa Laravel mengenali folder Controllers gaya lama
-            Route::middleware('web')
-                ->namespace('App\Http\Controllers')
-                ->group(base_path('routes/web.php'));
-        },
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
